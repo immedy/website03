475285512829ce3f1fdb1e3ed1064b818444df3b
Ragil M Rivandi <50643466+immedy@users.noreply.github.com>
2025-01-15 10:19:34 +0800

    -

--- resources/views/LandingPage/Konten/JadwalDokter/JadwalDokter.blade.php
+++ resources/views/LandingPage/Konten/JadwalDokter/JadwalDokter.blade.php
@@ -4,6 +4,28 @@
         <div class="container box_1170">
             <div class="section-top-border">
                 <h3 class="mb-30">Jadwal Dokter</h3>
+                <div class="col-md-4">
+                    <div class="typography">
+                        <h6>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-check2-all" viewBox="0 0 16 16">
+                                <path
+                                    d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0" />
+                                <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708" />
+                            </svg>
+                            Poli Buka
+                        </h6>
+
+                        <h6>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-x-lg" viewBox="0 0 16 16">
+                                <path
+                                    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                            </svg>
+                            Poli Tutup/Libur/Hari Besar
+                        </h6>
+                    </div>
+                </div>
                 <div class="progress-table-wrap">
                     <div class="progress-table">
                         <table class="table">
@@ -118,6 +140,11 @@
                             </tbody>
                         </table>
                     </div>
+                </div>
+            </div>
+            <div class="section-top-border">
+                <div class="row">
+
                 </div>
             </div>
         </div>
